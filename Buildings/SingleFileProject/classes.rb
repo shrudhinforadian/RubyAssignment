ea6711f0556e430728599a42cee54858c31ef88a
commonman007 <shrudhinvraj1998@gmail.com>
2019-11-27 16:04:14 +0530

    Single File Project Model Updated

--- Buildings/SingleFileProject/classes.rb
+++ Buildings/SingleFileProject/classes.rb
@@ -21,38 +21,30 @@ class Tenant
   # require_relative ('./modules')
   include CreditRating
   attr_reader :name, :age, :credit_score
-<<<<<<< HEAD
   @all = []
-=======
   @all=[]
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
   self.class.public_send(:attr_reader, :all)
   def initialize(name, age, credit_score)
     @name = name
     @age = age
     @credit_score = credit_score
     self.class.all << self
-<<<<<<< HEAD
     puts 'Tenant inserted successfully New Tenant list'
-=======
     puts "Tenant inserted successfully New Tenant list"
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
     self.class.all.each do |data|
       puts "Name : #{data.name} \tAge : #{data.age} \tCredit_score : #{data.credit_score}"
     end
   end
 end
 class Apartment
-<<<<<<< HEAD
+
   include CreditRating
   attr_reader :apartment_no, :rent, :sq_ft, :bedroom_count, :bathroom_count, :tenants
   @all = []
-=======
   # require_relative ('./modules')
   include CreditRating
   attr_reader :apartment_no, :rent, :sq_ft, :bedroom_count, :bathroom_count, :tenants
   @all=[]
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
   self.class.public_send(:attr_reader, :all)
   def initialize(apartment_no, rent, sq_ft, bedroom_count, bathroom_count)
     @apartment_no = apartment_no
@@ -62,18 +54,15 @@ class Apartment
     @bathroom_count = bathroom_count
     @tenants = []
     self.class.all << self
-<<<<<<< HEAD
     puts 'Apartment inserted successfully New Apartment list'
     self.class.all.each do |data|
       puts "Apartment#{data.apartment_no} \tRent : #{data.rent} \tSquare Feet : #{data.sq_ft} \tBedrooms : #{data.bedroom_count} \tBathrooms : #{data.bathroom_count}"
     end
-=======
     puts "Apartment inserted successfully New Apartment list"
     self.class.all.each do |data|
       puts "Apartment#{data.apartment_no} \tRent : #{data.rent} \tSquare Feet : #{data.sq_ft} \tBedrooms : #{data.bedroom_count} \tBathrooms : #{data.bathroom_count}"
     end
 
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
   end
 
   def add_tenant(tenant)
@@ -90,7 +79,6 @@ class Apartment
   end
 
   def remove_tenant(name)
-<<<<<<< HEAD
     flag = 0
     if @tenants.each do |tenant_check|
       flag = if tenant_check.name == name
@@ -108,7 +96,6 @@ class Apartment
       else
         puts "Tenant #{name} does not exist"
       end
-=======
     flag=0
     if @tenants.each do |tenant_check|
       if tenant_check.name == name
@@ -126,7 +113,6 @@ class Apartment
       else
         puts "Tenant #{name} does not exist"
       end
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
   end
 end
 
@@ -144,27 +130,21 @@ end
   end
 end
 class Building
-<<<<<<< HEAD
   @all = []
-=======
   # require_relative './modules'
   @all=[]
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
   attr_reader :address
   self.class.public_send(:attr_reader, :all)
   def initialize(address)
     @address = address
     @apartments = []
     self.class.all << self
-<<<<<<< HEAD
     puts 'Building inserted successfully New Building list'
     self.class.all.each do |data|
       puts data.address.to_s
-=======
     puts "Building inserted successfully New Building list"
     self.class.all.each do |data|
       puts "#{data.address}"
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
     end
   end
 
@@ -177,7 +157,6 @@ class Building
   end
 
   def remove_apartment(apartment_no)
-<<<<<<< HEAD
     flag = 0
     if @apartments.each do |apartment_check|
       if apartment_check.apartment_no == apartment_no && apartment_check.tenants.empty?
@@ -185,7 +164,6 @@ class Building
       else
         flag = 0
       end
-=======
     flag=0
     if @apartments.each do |apartment_check|
       if apartment_check.apartment_no == apartment_no && apartment_check.tenants.empty?
@@ -202,7 +180,6 @@ class Building
       end
     else
       puts "Apartment#{apartment_no} is not empty or apartment number is invalid"
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
     end
       if flag == 1
         @apartments.delete_if { |apartments| apartments.apartment_no == apartment_no }
@@ -250,11 +227,8 @@ tenant5 = Tenant.new('Twinkle', 20, 555)
 tenant6 = Tenant.new('Amal', 28, 455)
 apartment1 = Apartment.new(1, 6500, 5000, 90, 60)
 apartment2 = Apartment.new(2, 6000, 4800, 80, 50)
-<<<<<<< HEAD
-apartment3 = Apartment.new(3, 4000, 3000, 50, 20)
-=======
+apartment3 = Apartment.new(3, 4000, 3000, 50, 20
 apartment3=Apartment.new(3,4000,3000,50,20)
->>>>>>> dd0080d528ef4b5aa3925e22bde36093e25a18e6
 apartment1.add_tenant(tenant1)
 apartment1.add_tenant(tenant2)
 apartment1.add_tenant(tenant6)
