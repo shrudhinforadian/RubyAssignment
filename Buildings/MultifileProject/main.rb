bb78fa36f3bab47a91c332d428f6be72e8513bf4
commonman007 <shrudhinvraj1998@gmail.com>
2019-11-27 16:05:52 +0530

    Updated main

--- Buildings/MultifileProject/main.rb
+++ Buildings/MultifileProject/main.rb
@@ -1,8 +1,8 @@
 
-require_relative ('./libs/modules')
-require_relative ('./libs/apartment')
-require_relative ('./libs/buildings')
-require_relative ('./libs/tenant')
+require_relative './libs/modules'
+require_relative './libs/apartment'
+require_relative './libs/buildings'
+require_relative './libs/tenant'
 tenant1 = Tenant.new('Shrudhu', 21, 855)
 tenant2 = Tenant.new('Shreyas', 22, 755)
 tenant3 = Tenant.new('Snehith', 24, 815)
@@ -11,7 +11,7 @@ tenant5 = Tenant.new('Twinkle', 20, 555)
 tenant6 = Tenant.new('Amal', 28, 455)
 apartment1 = Apartment.new(1, 6500, 5000, 90, 60)
 apartment2 = Apartment.new(2, 6000, 4800, 80, 50)
-apartment3=Apartment.new(3,4000,3000,50,20)
+apartment3 = Apartment.new(3, 4000, 3000, 50, 20)
 apartment1.add_tenant(tenant1)
 apartment1.add_tenant(tenant2)
 apartment1.add_tenant(tenant6)
@@ -29,11 +29,6 @@ apartment1.remove_tenant(tenant2.name)
 apartment1.remove_tenant(tenant1.name)
 building1.remove_apartment(1)
 building1.remove_apartment(3)
-# puts building1.total_tenants
-# puts building2.total_revenue
-# puts apartment1.avg_credit
-# puts building2.remove_apartment(apartment2)
-# puts building1.remove_apartment(apartment1)
-# puts apartment1.remove_tenant('Amal')
-# puts apartment1.remove_tenant('Shrudhin')
- building1.total_tenants
+building2.total_revenue
+apartment1.avg_credit
+building1.total_tenants
